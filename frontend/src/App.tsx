ff11f2a8355ed4391976aa15935802cc246b777e
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-09-19 14:34:19 +0000

    feat: add app div container and update header styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,11 @@ import "./index.css"
 
 export default function App() {
     return (
-        <>
-            <header>
-                <h1 className="text-3xl">Tangled</h1>
-                <h2>Unravel Your Tasks, Stay Organized.</h2>
+        <div className="App">
+            <header className="relative top-20 text-center">
+                <h1 className="text-5xl text-gray-200">Tangled</h1>
+                <h2 className="text-gray-400 py-4">Unravel Your Tasks, Stay Organized.</h2>
             </header>
-        </>
+        </div>
     );
 }
