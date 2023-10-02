ad77f643c4bfd616af00b4c8917694c1cdcff770
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-02 12:00:57 +0000

    feat: add profile dropdown markup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,35 @@
 import './index.css';
 import { CgMenu } from 'react-icons/cg';
-import { AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineSearch, AiOutlineUser } from 'react-icons/ai';
 import { IoClose } from 'react-icons/io5';
+import { BiLogOut } from 'react-icons/bi';
+import { MdOutlineDarkMode } from 'react-icons/md';
 
 export default function App() {
     return (
         <div className="App">
             <header className="bg-boson-blue pt-10 px-5 lg:px-50 lg:pb-22">
                 <div className="flex justify-end">
-                    <div className="bg-lepton-lilac w-10 h-10 rounded-full hidden lg:flex items-center justify-center hover:cursor-pointer hover:border-2 hover:border-gray-100 transition-all">
-                        <span className="text-gray-100">P</span>
+                    <div className="hidden lg:block relative">
+                        <div className="bg-lepton-lilac w-10 h-10 rounded-full flex items-center justify-center hover:cursor-pointer hover:border-2 hover:border-gray-100 transition-all">
+                            <span className="text-gray-100">P</span>
+                        </div>
+                        <ul className="w-[200px] p-1.5 bg-white rounded-md flex flex-col gap-1 absolute top-10 right-0">
+                            <li className="px-4 py-1.5 text-gray-600 hover:bg-gray-100 rounded-md hover:cursor-pointer flex items-center gap-2">
+                                <AiOutlineUser size={20} />
+                                <span>Profile</span>
+                            </li>
+                            <li className="px-4 py-1.5 text-gray-600 hover:bg-gray-100 rounded-md hover:cursor-pointer flex items-center gap-2">
+                                <MdOutlineDarkMode size={20} />
+                                Dark Mode
+                            </li>
+                            <li className="px-4 py-1.5 text-gray-600 hover:bg-gray-100 rounded-md hover:cursor-pointer flex items-center gap-2">
+                                <BiLogOut size={20} />
+                                <span>Log out</span>
+                            </li>
+                        </ul>
                     </div>
+
                     <CgMenu size={28} className="text-gray-100 lg:hidden" />
                 </div>
                 <div className="flex flex-col items-center">
