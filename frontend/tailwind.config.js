6c99a33db09371be5daedb7d86360a901e6f5a89
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-02 11:11:30 +0000

    chore: use tailwindcss plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssPlugin from 'tailwindcss/plugin';
+
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
@@ -7,13 +9,17 @@ export default {
                 sans: ['Open sans', 'sans-serif'],
             },
             colors: {
-              'quark-purple': '#3e3148',
-              'lepton-lilac': '#9c7481',
-              'boson-blue': '#141530',
-              'neutrino-black': '#040404',
-              'gluon-olive': '#14140c',
-          },
+                'quark-purple': '#3e3148',
+                'lepton-lilac': '#9c7481',
+                'boson-blue': '#141530',
+                'neutrino-black': '#040404',
+                'gluon-olive': '#14140c',
+            },
         },
     },
-    plugins: [],
+    plugins: [
+        tailwindcssPlugin(({ addVariant }) => {
+            addVariant('search-cancel', '&::-webkit-search-cancel-button');
+        }),
+    ],
 };
