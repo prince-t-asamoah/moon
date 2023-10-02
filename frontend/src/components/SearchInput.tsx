4016df7114cae9731778ae2d278a31704ddab3e5
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-02 14:07:25 +0000

    style: change search and cancel button icon size

--- frontend/src/components/SearchInput.tsx
+++ frontend/src/components/SearchInput.tsx
@@ -14,12 +14,12 @@ export default function SearchInput({
 }: SearchInputProps) {
     return (
         <div className="w-full md:w-8/12 lg:w-6/12 bg-white px-2.5 rounded-md shadow-md flex items-center justify-center relative top-5 focus-within:ring-2 focus-within:ring-lepton-lilac">
-            <span>
-                <AiOutlineSearch size={30} className="text-gray-400" />
+            <span className="flex items-center justify-center w-7 h-7">
+                <AiOutlineSearch className="text-gray-400 w-full h-full" />
             </span>
             <input
                 type="search"
-                className="p-1.5 h-[3rem] w-full focus:outline-none placeholder:text-gray-400 search-cancel:appearance-none"
+                className="text-sm p-1.5 h-[3rem] w-full focus:outline-none placeholder:text-gray-400 search-cancel:appearance-none"
                 placeholder="Search for tasks"
                 value={value}
                 onChange={handleOnChange}
@@ -31,7 +31,7 @@ export default function SearchInput({
                     className="p-1 text-gray-500 lg:hover:text-gray-600 bg-gray-100 rounded-full transition-colors"
                     onClick={handleCancel}
                 >
-                    <IoClose size={20} />
+                    <IoClose size={18} />
                 </button>
             )}
         </div>
