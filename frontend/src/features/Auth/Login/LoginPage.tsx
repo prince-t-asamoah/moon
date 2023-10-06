f930f9c2551ef8d8cf88ebbd15fc162cb0c26ad6
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-06 16:29:36 +0000

    use auth navbar component

--- frontend/src/features/Auth/Login/LoginPage.tsx
+++ frontend/src/features/Auth/Login/LoginPage.tsx
@@ -1,23 +1,14 @@
-import { FaChevronLeft } from 'react-icons/fa6';
-import { Link } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import AuthFormFooter from '../components/AuthFormFooter';
 import AuthFormHeader from '../components/AuthFormHeader';
+import AuthNavBar from '../components/AuthNavBar';
 
 export default function LoginPage() {
     return (
         <div className="min-h-screen bg-gray-100 flex">
             <div className="lg:block flex-1 hidden bg-boson-blue"></div>
             <div className="flex-1 flex flex-col lg:gap-8 items-center relative">
-                <div className="px-5 py-8 md:p-8 lg:px-10 lg:pt-10 w-full">
-                    <Link
-                        to="/"
-                        className="font-semibold text-boson-blue lg:hover:underline inline-flex items-center gap-2"
-                    >
-                        <FaChevronLeft />
-                        <span>Back</span>
-                    </Link>
-                </div>
+                <AuthNavBar path="/" />
                 <div className="flex-grow py-4 w-full flex justify-center items-center">
                     <div className="w-4/5 md:w-3/5">
                         <AuthFormHeader
