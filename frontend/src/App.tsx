5d0b8acdb2d870eee1911b6f73f9d6454c8f1b58
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-06 16:18:23 +0000

    add forgot password route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,14 @@ import { Route, Routes } from 'react-router-dom';
 import LoginPage from './features/Auth/Login/LoginPage';
 import SignUpPage from './features/Auth/Signup/SignupPage';
 import DashboardPage from './DashboardPage';
+import ForgotPasswordPage from './features/Auth/ForgotPassword/ForgotPasswordPage';
 
 export default function App() {
     return (
         <Routes>
             <Route path="/signup" element={<SignUpPage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
     );
