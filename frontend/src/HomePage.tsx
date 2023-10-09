dfc4cf522033fae31089994ba72b1e506ea24b00
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-09 16:32:52 +0000

    update header and button layout

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -27,32 +27,34 @@ export default function HomePage() {
                 </ul>
             </nav>
             <main className="flex flex-col items-center flex-grow">
-                <header className="text-center">
-                    <h1 className="font-bold text-2xl md:text-3xl lg:text-5xl py-3 lg:py-5">
-                        An intuitive way to manage your tasks.
-                    </h1>
-                    <h2 className="text-gray-500 md:text-xl lg:text-2xl">
-                        Click on get started to sigup for free.
-                    </h2>
-                </header>
-                <div className="py-5 flex gap-2">
-                    <Button
-                        className="bg-quark-purple lg:hover:bg-boson-blue text-white font-bold lg:transition-colors"
-                        size="lg"
-                        as={Link}
-                        to="/signup"
-                    >
-                        Get Started
-                    </Button>
-                    <Button
-                        size="lg"
-                        className="text-quark-purple lg:hover:bg-gray-400 border-2 border-quark-purple"
-                        as={Link}
-                        to="/login"
-                        variant="ghost"
-                    >
-                        Login
-                    </Button>
+                <div className="mt-40 md:mt-72 lg:mt-48">
+                    <header className="text-center">
+                        <h1 className="font-bold text-2xl md:text-3xl lg:text-5xl py-3 lg:py-5">
+                            An intuitive way to manage your tasks.
+                        </h1>
+                        <h2 className="text-gray-500 md:text-xl lg:text-2xl">
+                            Click on get started to sigup for free.
+                        </h2>
+                    </header>
+                    <div className="py-5 flex justify-center gap-2">
+                        <Button
+                            className="bg-quark-purple lg:hover:bg-boson-blue text-white font-bold lg:transition-colors"
+                            size="lg"
+                            as={Link}
+                            to="/signup"
+                        >
+                            Get Started
+                        </Button>
+                        <Button
+                            size="lg"
+                            className="text-quark-purple lg:hover:bg-gray-400 border-2 border-quark-purple"
+                            as={Link}
+                            to="/login"
+                            variant="ghost"
+                        >
+                            Login
+                        </Button>
+                    </div>
                 </div>
             </main>
         </div>
