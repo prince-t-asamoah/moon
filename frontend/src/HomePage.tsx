acbdcea170eb3ffd64c7c6faa8bc4e6ec2b9d153
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-09 14:53:08 +0000

    add background image for homepage

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,17 +1,26 @@
+import HomeBackgroundImage from './assets/homepage-bg.svg';
+
 export default function HomePage() {
     return (
-        <main className="min-h-screen">
-            <header>
-                <h1 className="font-bold text-2xl">
-                    An intuitive way to manage your tasks.
-                </h1>
-                <h2 className="text-gray-400">Features include:</h2>
-                <ul>
-                    <li>Tasks</li>
-                    <li>Reminders</li>
-                    <li>Milestones</li>
-                </ul>
-            </header>
-        </main>
+        <div
+            style={{
+                background: `url(${HomeBackgroundImage}) no-repeat`,
+                backgroundSize: 'cover',
+            }}
+        >
+            <main className="min-h-screen">
+                <header>
+                    <h1 className="font-bold text-2xl">
+                        An intuitive way to manage your tasks.
+                    </h1>
+                    <h2 className="text-gray-400">Features include:</h2>
+                    <ul>
+                        <li>Tasks</li>
+                        <li>Reminders</li>
+                        <li>Milestones</li>
+                    </ul>
+                </header>
+            </main>
+        </div>
     );
 }
