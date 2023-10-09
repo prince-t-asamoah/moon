1ce9233c99b78b2f4761e44d828ff5b513a2a07b
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-09 14:06:07 +0000

    add image for forgot password page left column

--- frontend/src/features/Auth/ForgotPassword/ForgotPasswordPage.tsx
+++ frontend/src/features/Auth/ForgotPassword/ForgotPasswordPage.tsx
@@ -2,11 +2,12 @@ import ForgotPasswordForm from './ForgotPasswordForm';
 import AuthFormHeader from '../components/AuthFormHeader';
 import AuthNavBar from '../components/AuthNavBar';
 import AuthPageLeftColumn from '../components/AuthPageLeftColumn';
+import ForgotPasswordBgImage from '../assets/forgot-password-bg.svg';
 
 export default function ForgotPasswordPage() {
     return (
         <div className="min-h-screen bg-gray-100 flex">
-            <AuthPageLeftColumn/>
+            <AuthPageLeftColumn bgImage={ForgotPasswordBgImage} />
             <div className="flex-1 flex flex-col lg:gap-8 items-center relative">
                 <AuthNavBar path="/login" />
                 <div className="flex-grow py-4 w-full flex justify-center items-center">
