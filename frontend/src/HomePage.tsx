3b24b6dbf6a1c26e4d2a66a6be64869e912bc491
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-09 14:47:10 +0000

    add header for homepage

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,3 +1,17 @@
 export default function HomePage() {
-    return <div>HomePage</div>;
+    return (
+        <main className="min-h-screen">
+            <header>
+                <h1 className="font-bold text-2xl">
+                    An intuitive way to manage your tasks.
+                </h1>
+                <h2 className="text-gray-400">Features include:</h2>
+                <ul>
+                    <li>Tasks</li>
+                    <li>Reminders</li>
+                    <li>Milestones</li>
+                </ul>
+            </header>
+        </main>
+    );
 }
