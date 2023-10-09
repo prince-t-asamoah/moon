350d877785d450b4b447de73ada2881edeea560f
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-09 16:05:44 +0000

    style button backgrounds

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@nextui-org/button';
+import { Link } from 'react-router-dom';
 import HomeBackgroundImage from './assets/homepage-bg.svg';
 
 export default function HomePage() {
@@ -20,12 +21,22 @@ export default function HomePage() {
                 </header>
                 <div className="py-5 flex gap-2">
                     <Button
-                        className="bg-boson-blue text-white font-bold"
+                        className="bg-quark-purple lg:hover:bg-boson-blue text-white font-bold lg:transition-colors"
                         size="lg"
+                        as={Link}
+                        to="/signup"
                     >
                         Get Started
                     </Button>
-                    <Button size="lg">Login</Button>
+                    <Button
+                        size="lg"
+                        className="text-quark-purple lg:hover:bg-gray-400 border-2 border-quark-purple"
+                        as={Link}
+                        to="/login"
+                        variant="ghost"
+                    >
+                        Login
+                    </Button>
                 </div>
             </main>
         </div>
