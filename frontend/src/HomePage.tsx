dcf4a5ce8dd2af39acbb07912da04f547b7b618c
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-09 15:37:03 +0000

    add signup and login button

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@nextui-org/button';
 import HomeBackgroundImage from './assets/homepage-bg.svg';
 
 export default function HomePage() {
@@ -8,18 +9,24 @@ export default function HomePage() {
                 backgroundSize: 'cover',
             }}
         >
-            <main className="min-h-screen">
-                <header>
-                    <h1 className="font-bold text-2xl">
+            <main className="min-h-screen flex flex-col items-center justify-center">
+                <header className="text-center">
+                    <h1 className="font-bold text-2xl md:text-3xl lg:text-5xl py-3 lg:py-5">
                         An intuitive way to manage your tasks.
                     </h1>
-                    <h2 className="text-gray-400">Features include:</h2>
-                    <ul>
-                        <li>Tasks</li>
-                        <li>Reminders</li>
-                        <li>Milestones</li>
-                    </ul>
+                    <h2 className="text-gray-500 md:text-xl lg:text-2xl">
+                        Click on get started to sigup for free.
+                    </h2>
                 </header>
+                <div className="py-5 flex gap-2">
+                    <Button
+                        className="bg-boson-blue text-white font-bold"
+                        size="lg"
+                    >
+                        Get Started
+                    </Button>
+                    <Button size="lg">Login</Button>
+                </div>
             </main>
         </div>
     );
