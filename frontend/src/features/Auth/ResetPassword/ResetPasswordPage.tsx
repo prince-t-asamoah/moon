48f235b1eeb452612dbefc71b7b6c0549c1df05c
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-09 14:07:29 +0000

    add reset password background image left column

--- frontend/src/features/Auth/ResetPassword/ResetPasswordPage.tsx
+++ frontend/src/features/Auth/ResetPassword/ResetPasswordPage.tsx
@@ -1,11 +1,12 @@
 import AuthFormHeader from '../components/AuthFormHeader';
 import AuthPageLeftColumn from '../components/AuthPageLeftColumn';
 import ResetPasswordForm from './ResetPasswordForm';
+import ResetPasswordBgImage from '../assets/reset-password-bg.svg';
 
 export default function ResetPasswordPage() {
     return (
         <div className="min-h-screen bg-gray-100 flex">
-            <AuthPageLeftColumn />
+            <AuthPageLeftColumn bgImage={ResetPasswordBgImage} />
             <div className="flex-1 flex flex-col lg:gap-8 items-center relative">
                 <div className="flex-grow py-4 w-full flex justify-center items-center">
                     <div className="w-4/5 md:w-3/5">
