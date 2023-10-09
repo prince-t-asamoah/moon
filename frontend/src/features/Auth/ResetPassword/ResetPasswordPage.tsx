42ef682e088b34fc299951c2c5a66e94b5e25022
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-09 09:14:56 +0000

    create reset password page

---
+++ frontend/src/features/Auth/ResetPassword/ResetPasswordPage.tsx
@@ -0,0 +1,22 @@
+import AuthFormHeader from '../components/AuthFormHeader';
+import AuthPageLeftColumn from '../components/AuthPageLeftColumn';
+import ResetPasswordForm from './ResetPasswordForm';
+
+export default function ResetPasswordPage() {
+    return (
+        <div className="min-h-screen bg-gray-100 flex">
+            <AuthPageLeftColumn />
+            <div className="flex-1 flex flex-col lg:gap-8 items-center relative">
+                <div className="flex-grow py-4 w-full flex justify-center items-center">
+                    <div className="w-4/5 md:w-3/5">
+                        <AuthFormHeader
+                            title="Set new password"
+                            subTitle="Your new password must be different from the previous password"
+                        />
+                        <ResetPasswordForm />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
