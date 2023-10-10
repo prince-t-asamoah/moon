f5bab11495be1311702476be04a1cbbccecab31c
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-10 12:54:29 +0000

    add app nav bar for mobile and tablet screens

--- frontend/src/DashboardPage.tsx
+++ frontend/src/DashboardPage.tsx
@@ -1,8 +1,26 @@
+import { NavLink } from 'react-router-dom';
+import { GoHome } from 'react-icons/go';
+
 export default function DashboardPage() {
-  return (
-    <div className="h-screen relative">
-      <aside className="bg-quark-purple absolute left-0 top-0 h-full w-2/12">Aside</aside>
-      <main className="bg-gray-100 absolute right-0 top-0 bottom-0 left-[16.67%]"> Main</main>
-    </div>
-  )
+    return (
+        <div className="h-screen relative">
+            <aside className="w-full p-5 fixed bottom-0 z-10 bg-white rounded-t-2xl">
+                <nav>
+                    <ul className="flex justify-center">
+                        <li className="flex items-center justify-center">
+                            <NavLink
+                                to="/dashboard"
+                                className="p-2 rounded-md text-gray-600"
+                            >
+                                <GoHome size={28} />
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+            <main className="bg-gray-100 absolute right-0 top-0 bottom-0 lg:left-[16.67%] left-0">
+                Main
+            </main>
+        </div>
+    );
 }
