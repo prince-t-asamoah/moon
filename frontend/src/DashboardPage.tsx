7be50af1800131c0593c77d1ba2fc616df16fc41
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-10 11:16:58 +0000

    add sidebar and main content area

--- frontend/src/DashboardPage.tsx
+++ frontend/src/DashboardPage.tsx
@@ -1,5 +1,8 @@
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
+    <div className="h-screen relative">
+      <aside className="bg-quark-purple absolute left-0 top-0 h-full w-2/12">Aside</aside>
+      <main className="bg-gray-100 absolute right-0 top-0 bottom-0 left-[16.67%]"> Main</main>
+    </div>
   )
 }
