79a1a38d8e00cb3121b14b76a23ccfaeeeb79f24
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-10 15:44:39 +0000

    create a navbar component for dashboard page

--- frontend/src/DashboardPage.tsx
+++ frontend/src/DashboardPage.tsx
@@ -1,49 +1,11 @@
-import { NavLink } from 'react-router-dom';
 import { GoHome } from 'react-icons/go';
 import { BsChevronDown } from 'react-icons/bs';
-
-const defaultLinkStyle =
-    'text-gray-100 px-5 py-2 flex items-center gap-2 hover:bg-gray-500 rounded-md';
+import DashboardNavBar from './components/DashboardNavBar';
 
 export default function DashboardPage() {
     return (
         <div className="h-screen relative">
-            <aside className="bg-boson-blue absolute left-0 top-0 h-full w-2/12 hidden lg:flex flex-col">
-                <div className="px-4 py-4">
-                    <span className="text-gray-100 text-xl">Task Manager</span>
-                </div>
-                <nav className="flex-grow">
-                    <ul className="flex flex-col gap-1.5 py-8">
-                        <li className="px-3">
-                            <NavLink
-                                to="/dashboard"
-                                className={({ isActive }) => {
-                                    return isActive
-                                        ? defaultLinkStyle + ' bg-gray-500'
-                                        : defaultLinkStyle;
-                                }}
-                            >
-                                <GoHome size={20} />
-                                <span>Home</span>
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </aside>
-            <aside className="w-full p-5 fixed bottom-0 z-10 bg-white rounded-t-2xl lg:hidden">
-                <nav>
-                    <ul className="flex justify-center">
-                        <li className="flex items-center justify-center">
-                            <NavLink
-                                to="/dashboard"
-                                className="p-2 rounded-md text-gray-600"
-                            >
-                                <GoHome size={28} />
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </aside>
+            <DashboardNavBar />
             <main className="bg-gray-100 absolute right-0 top-0 bottom-0 lg:left-[16.67%] left-0">
                 <header className="w-full border-b px-8 py-3 sticky top-0 justify-between hidden lg:flex">
                     <h1 className="text-gray-700 font-semibold flex items-center gap-2">
@@ -65,11 +27,11 @@ export default function DashboardPage() {
                         </button>
                     </div>
                 </header>
-                <div>
+                <section>
                     <h2 className="text-gray-700 font-bold text-lg lg:text-2xl px-5 py-4 md:p-8 flex items-center gap-2">
                         Home
                     </h2>
-                </div>
+                </section>
             </main>
         </div>
     );
---
+++ frontend/src/components/DashboardNavBar.tsx
@@ -0,0 +1,48 @@
+import { GoHome } from 'react-icons/go';
+import { NavLink } from 'react-router-dom';
+
+const defaultLinkStyle =
+    'text-gray-100 px-5 py-2 flex items-center gap-2 hover:bg-gray-500 rounded-md';
+
+export default function DashboardNavBar() {
+    return (
+        <>
+            <aside className="bg-boson-blue absolute left-0 top-0 h-full w-2/12 hidden lg:flex flex-col">
+                <div className="px-4 py-4">
+                    <span className="text-gray-100 text-xl">Task Manager</span>
+                </div>
+                <nav className="flex-grow">
+                    <ul className="flex flex-col gap-1.5 py-8">
+                        <li className="px-3">
+                            <NavLink
+                                to="/dashboard"
+                                className={({ isActive }) => {
+                                    return isActive
+                                        ? defaultLinkStyle + ' bg-gray-500'
+                                        : defaultLinkStyle;
+                                }}
+                            >
+                                <GoHome size={20} />
+                                <span>Home</span>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+            <aside className="w-full p-5 fixed bottom-0 z-10 bg-white rounded-t-2xl lg:hidden">
+                <nav>
+                    <ul className="flex justify-center">
+                        <li className="flex items-center justify-center">
+                            <NavLink
+                                to="/dashboard"
+                                className="p-2 rounded-md text-gray-600"
+                            >
+                                <GoHome size={28} />
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+        </>
+    );
+}
