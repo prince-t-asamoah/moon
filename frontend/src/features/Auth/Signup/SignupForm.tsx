2e080e5e669861697f1c4db8c3773b44b2b2066e
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-13 11:54:59 +0000

    install yup for form input validation

--- frontend/src/features/Auth/Signup/SignupForm.tsx
+++ frontend/src/features/Auth/Signup/SignupForm.tsx
@@ -1,13 +1,26 @@
 import { Button, Input } from '@nextui-org/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 const formInputStyles = {
     label: 'lg:text-[0.9375rem] lg:py-1.5 text-gray-950 font-semibold',
     input: 'lg:text-medium',
 };
 
+interface SignupFormData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
 export default function SignupForm() {
+    const { register, handleSubmit } = useForm<SignupFormData>();
+
+    const handleSignup: SubmitHandler<SignupFormData> = () => {};
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(handleSignup)}>
             <div className="flex flex-col gap-5 lg:gap-1">
                 <div className="flex flex-col gap-4 md:py-5">
                     <Input
@@ -15,33 +28,39 @@ export default function SignupForm() {
                         label="First Name"
                         placeholder="Enter your first name"
                         classNames={formInputStyles}
+                        {...register('firstName')}
                     />
                     <Input
                         type="text"
                         label="Last Name"
                         placeholder="Enter your last name"
                         classNames={formInputStyles}
+                        {...register('lastName')}
                     />
                     <Input
                         type="email"
                         label="Email"
                         placeholder="Enter your email"
                         classNames={formInputStyles}
+                        {...register('email')}
                     />
                     <Input
                         type="password"
                         label="Password"
                         placeholder="Enter your password"
                         classNames={formInputStyles}
+                        {...register('password')}
                     />
                     <Input
                         type="password"
                         label="Confirm Password"
                         placeholder="Enter your password again"
                         classNames={formInputStyles}
+                        {...register('confirmPassword')}
                     />
                 </div>
                 <Button
+                    type="submit"
                     size="lg"
                     radius="sm"
                     className="font-semibold bg-boson-blue lg:hover:opacity-70 lg:transition-all text-gray-100"
