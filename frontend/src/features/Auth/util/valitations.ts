8485250feafb4a61b7ca6040b09de702ae964992
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-13 14:09:17 +0000

    create form schema for validations

---
+++ frontend/src/features/Auth/util/valitations.ts
@@ -0,0 +1,22 @@
+import * as yup from 'yup';
+
+export const schema = yup.object().shape({
+    firstName: yup.string().required('First Name is required'),
+    lastName: yup.string().required('Last Name is required'),
+    password: yup
+        .string()
+        .required('Password is required')
+        .matches(
+            /^(?=.*[a-zA-Z])(?=.*\d)/,
+            'Password must contain at least one letter and one number'
+        )
+        .min(8, 'Password must be at least 8 characters long'),
+    confirmPassword: yup
+        .string()
+        .required('Confirm password is required')
+        .oneOf([yup.ref('password')], 'Passwords must match'),
+    email: yup
+        .string()
+        .required('Email is required')
+        .email('Invalid email format'),
+});
