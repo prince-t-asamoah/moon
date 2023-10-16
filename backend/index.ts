4740fbde3db57db18e940a8a2cf2dc14f34b779e
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-16 14:26:46 +0000

    use home router

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import homeRoute from './routes/home';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -6,3 +7,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
     console.log(`Application server running on port ${PORT}`)
 );
+
+app.use("/", homeRoute);
