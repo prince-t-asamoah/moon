7bf58640b41e7ee8bafc61f6b3b80e119ebfe468
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-17 15:47:46 +0000

    type custom errors

--- backend/src/middlewares/authMiddlewares.ts
+++ backend/src/middlewares/authMiddlewares.ts
@@ -1,19 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import { signupSchema } from '../data/authSchemaData';
 
-export async function validateSignupData(
+export function validateSignupData(
     req: Request,
     res: Response,
     next: NextFunction
 ) {
-    const { error } = await signupSchema.validate(req.body);
+    const { error } = signupSchema.validate(req.body);
     if (error) {
-        const customErrors: any = [];
+        const customErrors: Array<{ status: string; message: string }> = [];
         error.details.forEach((detail) => {
             customErrors.push({ status: '400', message: detail.message });
         });
         return res.status(400).json({
-            error: customErrors,
+            errors: customErrors,
         });
     } else {
         next();
