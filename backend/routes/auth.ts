ddd66f6037fc34b576f930cd6f5a563d111a0465
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-17 11:50:01 +0000

    move to src routes directory

--- backend/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const authRoute = Router();
-
-authRoute.post('/signup', (req, res) => {
-    res.json(req.body);
-});
-
-export default authRoute;
---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import bcrypt from 'bcrypt';
+import { SignupDTO } from '../@types/authDTO';
+
+const authRoute = Router();
+
+authRoute.post('/signup', (req, res) => {
+    const { firstName, lastName, otherName, email, password } =
+        req.body as SignupDTO;
+
+    bcrypt.hash(password, 10, async function (err: any, hashPassword: string) {
+        if (err) {
+            return res.status(500).json({
+                error: {
+                    status: '500',
+                    title: 'Internal server error',
+                    description: 'Error while encrypting password',
+                },
+            });
+        }
+        res.status(201).json({
+            success: {
+                status: '201',
+                title: 'Account creation successful',
+                description: 'New user added successfully',
+            },
+            data: {
+                firstName,
+                lastName,
+                otherName,
+                email,
+                password: hashPassword,
+            },
+        });
+    });
+});
+
+export default authRoute;
