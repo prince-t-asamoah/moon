8c319d7292fd58d9cabb79e97f3d1a273c3cf737
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-17 14:29:46 +0000

    use validateSignupData middleware

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import bcrypt from 'bcrypt';
 import { SignupDTO } from '../@types/authDTO';
+import { validateSignupData } from '../middlewares/authMiddlewares';
 
 const authRoute = Router();
 
-authRoute.post('/signup', (req, res) => {
+authRoute.post('/signup', validateSignupData, (req, res) => {
     const { firstName, lastName, otherName, email, password } =
         req.body as SignupDTO;
-
     bcrypt.hash(password, 10, async function (err: any, hashPassword: string) {
         if (err) {
             return res.status(500).json({
