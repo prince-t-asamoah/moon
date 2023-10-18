89f159ec32a9f25c6547e90d73c1429b9bff4516
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-18 09:51:39 +0000

    use nullish coalescing operator for port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import homeRoute from './routes/home';
 import authRoute from './routes/auth';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ?? 5000;
 
 app.listen(PORT, () =>
     console.log(`Application server running on port ${PORT}`)
