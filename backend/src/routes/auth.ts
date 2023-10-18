96f707d4bf72c21690a411ee6d03225b14467a09
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-18 14:53:07 +0000

    implement login route

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { Prisma } from '@prisma/client';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-import { SignupDTO } from '../@types/authDTO';
-import { validateSignupData } from '../middlewares/authMiddlewares';
+import { LoginDTO, SignupDTO } from '../@types/authDTO';
+import {
+    validateLoginData,
+    validateSignupData,
+} from '../middlewares/authMiddlewares';
 import { db } from '../prisma/client';
 
 const authRoute = Router();
@@ -53,6 +57,46 @@ authRoute.post('/signup', validateSignupData, (req, res) => {
                 });
         }
     });
+});
+
+authRoute.post('/login', validateLoginData, async (req, res) => {
+    const { email, password } = req.body as LoginDTO;
+    const jwtSecret = process.env.JWT_SECRET ?? '';
+
+    try {
+        const user = await db.user.findUnique({ where: { email } });
+        if (!user) {
+            return res.status(400).json({
+                errors: [{ status: '400', message: 'Invalid credentials' }],
+            });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                errors: [{ status: '400', message: 'Invalid credentials' }],
+            });
+        }
+        const token = jwt.sign({ userId: user.id }, jwtSecret, {
+            expiresIn: '1h',
+        });
+
+        return res.status(200).json({
+            user: {
+                id: user.id,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                otherName: user.otherName,
+                profileImageUrl: user.profileImageUrl,
+            },
+            token,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: { status: '500', message: 'Interal server error' },
+        });
+    }
 });
 
 export default authRoute;
