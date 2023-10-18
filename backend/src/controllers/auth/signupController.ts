055b8ac25dcf0c2b128e05047e9337ae543fef86
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-18 15:17:59 +0000

    create controller function for signup

---
+++ backend/src/controllers/auth/signupController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express';
+import { Prisma } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+import { SignupDTO } from '../../@types/authDTO';
+import { db } from '../../prisma/client';
+
+export default function loginUser(req: Request, res: Response) {
+    const { firstName, lastName, email, password } = req.body as SignupDTO;
+
+    bcrypt.hash(password, 10, async function (err: any, hashPassword: string) {
+        if (err) {
+            return res.status(500).json({
+                errors: [
+                    {
+                        status: '500',
+                        message: 'Internal server error',
+                    },
+                ],
+            });
+        }
+
+        try {
+            const newUser = await db.user.create({
+                data: {
+                    firstName,
+                    lastName,
+                    email,
+                    password: hashPassword,
+                },
+            });
+            if (newUser) {
+                return res.status(201).json({
+                    success: {
+                        status: '201',
+                        message: 'Account creation successful',
+                    },
+                });
+            }
+        } catch (error) {
+            if (
+                error instanceof Prisma.PrismaClientKnownRequestError &&
+                error.code === 'P2002'
+            )
+                return res.status(500).json({
+                    error: {
+                        status: '500',
+                        message: 'Email already exist, try another email',
+                    },
+                });
+        }
+    });
+}
