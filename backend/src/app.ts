6945a053beed3be2441ae5d91c33fd52ae9d74b5
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-18 14:42:53 +0000

    use dotenv to load .env

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import homeRoute from './routes/home';
 import authRoute from './routes/auth';
+import dotenv from 'dotenv';
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
+dotenv.config();
 
 app.listen(PORT, () =>
     console.log(`Application server running on port ${PORT}`)
