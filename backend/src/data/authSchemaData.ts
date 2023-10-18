075150c41b3b8d7569a5c4f499a7a40f4a485baf
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-18 14:53:29 +0000

    add login schema validation

--- backend/src/data/authSchemaData.ts
+++ backend/src/data/authSchemaData.ts
@@ -1,30 +1,43 @@
-import Joi from "joi";
+import Joi from 'joi';
 
-export const signupSchema = Joi.object({
-    firstName: Joi.string().min(3).required().messages({
+const customMessages = {
+    firstName: {
         'string.base': 'First name should be a string',
-        'string.min':
-            'First name should be at least {#limit} characters long',
+        'string.min': 'First name should be at least {#limit} characters long',
         'any.required': 'First name is required',
-    }),
-    lastName: Joi.string().min(3).required().messages({
+    },
+    lastName: {
         'string.base': 'Last name should be a string',
-        'string.min':
-            'Last name should be at least {#limit} characters long',
+        'string.min': 'Last name should be at least {#limit} characters long',
         'any.required': 'Last name is required',
-    }),
-    email: Joi.string().email().required().messages({
+    },
+    email: {
         'string.base': 'Email should be a string',
         'string.email': 'Invalid email format',
         'any.required': 'Email is required',
-    }),
+    },
+    password: {
+        'string.base': 'Password should be a string',
+        'string.pattern.base':
+            'Password must contain at least one letter and one number',
+        'any.required': 'Password is required',
+    },
+};
+
+export const signupSchema = Joi.object({
+    firstName: Joi.string()
+        .min(3)
+        .required()
+        .messages(customMessages.firstName),
+    lastName: Joi.string().min(3).required().messages(customMessages.lastName),
+    email: Joi.string().email().required().messages(customMessages.email),
     password: Joi.string()
         .regex(/^(?=.*[a-zA-Z])(?=.*\d)/)
         .required()
-        .messages({
-            'string.base': 'Password should be a string',
-            'string.pattern.base':
-                'Password must contain at least one letter and one number',
-            'any.required': 'Password is required',
-        }),
+        .messages(customMessages.password),
+});
+
+export const loginSchema = Joi.object({
+    email: Joi.string().email().required().messages(customMessages.email),
+    password: Joi.string().required().messages(customMessages.password),
 });
