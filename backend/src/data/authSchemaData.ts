91c270635e90558224e2b5d6917331467b411a4d
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-18 14:59:24 +0000

    import and use validation messages

--- backend/src/data/authSchemaData.ts
+++ backend/src/data/authSchemaData.ts
@@ -1,43 +1,20 @@
 import Joi from 'joi';
-
-const customMessages = {
-    firstName: {
-        'string.base': 'First name should be a string',
-        'string.min': 'First name should be at least {#limit} characters long',
-        'any.required': 'First name is required',
-    },
-    lastName: {
-        'string.base': 'Last name should be a string',
-        'string.min': 'Last name should be at least {#limit} characters long',
-        'any.required': 'Last name is required',
-    },
-    email: {
-        'string.base': 'Email should be a string',
-        'string.email': 'Invalid email format',
-        'any.required': 'Email is required',
-    },
-    password: {
-        'string.base': 'Password should be a string',
-        'string.pattern.base':
-            'Password must contain at least one letter and one number',
-        'any.required': 'Password is required',
-    },
-};
+import validationMessages from './valiadtionMessages';
 
 export const signupSchema = Joi.object({
     firstName: Joi.string()
         .min(3)
         .required()
-        .messages(customMessages.firstName),
-    lastName: Joi.string().min(3).required().messages(customMessages.lastName),
-    email: Joi.string().email().required().messages(customMessages.email),
+        .messages(validationMessages.firstName),
+    lastName: Joi.string().min(3).required().messages(validationMessages.lastName),
+    email: Joi.string().email().required().messages(validationMessages.email),
     password: Joi.string()
         .regex(/^(?=.*[a-zA-Z])(?=.*\d)/)
         .required()
-        .messages(customMessages.password),
+        .messages(validationMessages.password),
 });
 
 export const loginSchema = Joi.object({
-    email: Joi.string().email().required().messages(customMessages.email),
-    password: Joi.string().required().messages(customMessages.password),
+    email: Joi.string().email().required().messages(validationMessages.email),
+    password: Joi.string().required().messages(validationMessages.password),
 });
