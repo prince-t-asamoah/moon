addddaa66aa9afd46f8c19ebb69ddfa3613bc4e6
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-26 15:39:20 +0000

    rename loginAPI to authAPI

---
+++ frontend/src/features/Auth/apis/authAPI.ts
@@ -0,0 +1,21 @@
+import { API_CLIENT } from '../../../api/config';
+import { handleAPIError } from '../../../api/util';
+import { LoginFormData, SignUpFormData } from '../types/authTypes';
+
+export const loginAPI = async (data: LoginFormData) => {
+    try {
+        const response = await API_CLIENT.post('/auth/signup', data);
+        return response;
+    } catch (error: any) {
+        throw handleAPIError(error);
+    }
+};
+
+export const signupAPI = async (data: SignUpFormData) => {
+    try {
+        const response = await API_CLIENT.post('/auth/login', data);
+        return response;
+    } catch (error: any) {
+        throw handleAPIError(error);
+    }
+};
