ed60f9ac7f05d50c05fa1cd135b44a101457b191
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-26 13:47:56 +0000

    add react hot toast container to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './index.css';
 import 'react-circular-progressbar/dist/styles.css';
 import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 import LoginPage from './features/Auth/Login/LoginPage';
 import SignUpPage from './features/Auth/Signup/SignupPage';
@@ -11,13 +12,19 @@ import HomePage from './pages/HomePage';
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/dashboard/*" element={<DashboardPage />} />
-        </Routes>
+        <>
+            <Toaster />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route
+                    path="/forgot-password"
+                    element={<ForgotPasswordPage />}
+                />
+                <Route path="/reset-password" element={<ResetPassword />} />
+                <Route path="/dashboard/*" element={<DashboardPage />} />
+            </Routes>
+        </>
     );
 }
