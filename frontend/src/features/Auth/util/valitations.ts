fefd4097c50de0346bda7ec7d389f76e1993fdf9
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-26 13:54:53 +0000

    create and export login schema

--- frontend/src/features/Auth/util/valitations.ts
+++ frontend/src/features/Auth/util/valitations.ts
@@ -1,5 +1,20 @@
 import * as yup from 'yup';
 
+export const loginSchema = yup.object().shape({
+    email: yup
+        .string()
+        .required('Email is required')
+        .email('Invalid email format'),
+    password: yup
+        .string()
+        .required('Password is required')
+        .matches(
+            /^(?=.*[a-zA-Z])(?=.*\d)/,
+            'Password must contain at least one letter and one number'
+        )
+        .min(8, 'Password must be at least 8 characters long'),
+});
+
 export const schema = yup.object().shape({
     firstName: yup.string().required('First Name is required'),
     lastName: yup.string().required('Last Name is required'),
