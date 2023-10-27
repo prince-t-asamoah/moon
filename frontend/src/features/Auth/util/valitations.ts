335c72f3d06e3e41e65faa58121050980edd7b8b
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-27 08:13:14 +0000

    add signup schema validation

--- frontend/src/features/Auth/util/valitations.ts
+++ frontend/src/features/Auth/util/valitations.ts
@@ -15,6 +15,27 @@ export const loginSchema = yup.object().shape({
         .min(8, 'Password must be at least 8 characters long'),
 });
 
+export const signupSchema = yup.object().shape({
+    firstName: yup.string().required('First Name is required'),
+    lastName: yup.string().required('Last Name is required'),
+    password: yup
+        .string()
+        .required('Password is required')
+        .matches(
+            /^(?=.*[a-zA-Z])(?=.*\d)/,
+            'Password must contain at least one letter and one number'
+        )
+        .min(8, 'Password must be at least 8 characters long'),
+    confirmPassword: yup
+        .string()
+        .required('Confirm password is required')
+        .oneOf([yup.ref('password')], 'Passwords must match'),
+    email: yup
+        .string()
+        .required('Email is required')
+        .email('Invalid email format'),
+});
+
 export const schema = yup.object().shape({
     firstName: yup.string().required('First Name is required'),
     lastName: yup.string().required('Last Name is required'),
