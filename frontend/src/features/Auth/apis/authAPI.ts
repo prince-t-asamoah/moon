603c364ef23f2717592b5589c58615b74dfa497c
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-27 08:11:04 +0000

    update login and signup api endpoint

--- frontend/src/features/Auth/apis/authAPI.ts
+++ frontend/src/features/Auth/apis/authAPI.ts
@@ -4,7 +4,7 @@ import { LoginFormData, SignUpFormData } from '../types/authTypes';
 
 export const loginAPI = async (data: LoginFormData) => {
     try {
-        const response = await API_CLIENT.post('/auth/signup', data);
+        const response = await API_CLIENT.post('/auth/login', data);
         return response;
     } catch (error: any) {
         throw handleAPIError(error);
@@ -13,7 +13,7 @@ export const loginAPI = async (data: LoginFormData) => {
 
 export const signupAPI = async (data: SignUpFormData) => {
     try {
-        const response = await API_CLIENT.post('/auth/login', data);
+        const response = await API_CLIENT.post('/auth/signup', data);
         return response;
     } catch (error: any) {
         throw handleAPIError(error);
