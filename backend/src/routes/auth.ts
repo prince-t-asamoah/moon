6dd19bdfa03c5951ec5b66a3a2d3ea415b8e4054
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-31 15:43:25 +0000

    add forgot password api route

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 
 import {
+    validateForgotPasswordData,
     validateLoginData,
     validateSignupData,
 } from '../middlewares/authMiddlewares';
 import signupController from '../controllers/auth/signupController';
 import loginController from '../controllers/auth/loginController';
+import forgotPasswordController from '../controllers/auth/forgotPasswordController';
 
 const authRoute = Router();
 
 authRoute.post('/signup', validateSignupData, signupController);
 authRoute.post('/login', validateLoginData, loginController);
+authRoute.post('/forgot-password', validateForgotPasswordData, forgotPasswordController);
 
 export default authRoute;
