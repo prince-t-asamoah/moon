0fd763c621d946cfd9870dc6cebebf50c6a23c67
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-10-31 16:03:33 +0000

    update error request handling

--- frontend/src/api/util.ts
+++ frontend/src/api/util.ts
@@ -4,9 +4,7 @@ export const handleAPIError = (
     error: AxiosError<{ error: { status: string; message: string } }>
 ) => {
     if (error.response) {
-        return error.response.data.error;
-    } else if (error.request) {
-        return 'Sending request failed, try again';
+        return error.response.data.error.message;
     } else {
         return error.message;
     }
