cf8e6f469a2a48bd5a600dd5aea383bee77878c4
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-11-01 10:22:47 +0000

    add verify reset password token route

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,16 +3,23 @@ import { Router } from 'express';
 import {
     validateForgotPasswordData,
     validateLoginData,
+    validateResetPasswordData,
     validateSignupData,
 } from '../middlewares/authMiddlewares';
 import signupController from '../controllers/auth/signupController';
 import loginController from '../controllers/auth/loginController';
 import forgotPasswordController from '../controllers/auth/forgotPasswordController';
+import { verifyResetPasswordToken } from '../controllers/auth/resetPasswordController';
 
 const authRoute = Router();
 
 authRoute.post('/signup', validateSignupData, signupController);
 authRoute.post('/login', validateLoginData, loginController);
-authRoute.post('/forgot-password', validateForgotPasswordData, forgotPasswordController);
+authRoute.post(
+    '/forgot-password',
+    validateForgotPasswordData,
+    forgotPasswordController
+);
+authRoute.get('/reset-password/verify', verifyResetPasswordToken);
 
 export default authRoute;
