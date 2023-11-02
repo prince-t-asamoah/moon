48556be00653eecfd409ff3869ceaebc6a5ac319
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2023-11-02 10:58:50 +0000

    update loading .env depending on node environment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from "cors";
+import cors from 'cors';
+import path from 'path';
+
 import homeRoute from './routes/home';
 import authRoute from './routes/auth';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
-dotenv.config();
+const environment = process.env.NODE_ENV ?? 'development';
+const envFile = environment === 'production' ? '.env' : '.env.local';
+
+dotenv.config({ path: path.join(__dirname, envFile) });
 
 app.listen(PORT, () =>
     console.log(`Application server running on port ${PORT}`)
 );
 
 app.use(express.json());
-app.use(cors({ origin: "*"}))
+app.use(cors({ origin: '*' }));
 app.use('/', homeRoute);
 app.use('/auth', authRoute);
 app.use(errorHandler);
